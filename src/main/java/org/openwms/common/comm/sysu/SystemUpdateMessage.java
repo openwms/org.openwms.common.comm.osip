180ecebc4d75c7e171af3bfe9819a29d9190e0a7
Heiko Scherrer <scherrer@openwms.org>
2018-11-22 15:15:39 +0100

    #24 added static routing feature and merged message handler implementations in order to become more generic in TMS routing - in COMM we need to pass the message type in order to get generic on routing side - SYSU Routing takes message type from message

--- src/main/java/org/openwms/common/comm/sysu/SystemUpdateMessage.java
+++ src/main/java/org/openwms/common/comm/sysu/SystemUpdateMessage.java
@@ -39,29 +39,23 @@ public class SystemUpdateMessage extends Payload implements Serializable {
 
     /** Message identifier {@value} . */
     public static final String IDENTIFIER = "SYSU";
+    private String type;
     private String locationGroupName;
 
     private SystemUpdateMessage(Builder builder) {
         locationGroupName = builder.locationGroupName;
     }
 
-    /**
-     * Get the name of the LocationGroup.
-     *
-     * @return The name
-     */
+    public String getType() {
+        return IDENTIFIER;
+    }
+
     public String getLocationGroupName() {
         return locationGroupName;
     }
 
-    public void setLocationGroupName(String locationGroupName) {
-        this.locationGroupName = locationGroupName;
-    }
-
     /**
-     * Subclasses have to return an unique, case-sensitive message identifier.
-     *
-     * @return The message TYPE field (see OSIP specification)
+     * {@inheritDoc}
      */
     @Override
     public String getMessageIdentifier() {
@@ -69,9 +63,7 @@ public class SystemUpdateMessage extends Payload implements Serializable {
     }
 
     /**
-     * Does this type of message needs to be replied to?
-     *
-     * @return {@literal true} no reply needed, otherwise {@literal false}
+     * {@inheritDoc}
      */
     @Override
     public boolean isWithoutReply() {
@@ -79,56 +71,27 @@ public class SystemUpdateMessage extends Payload implements Serializable {
     }
 
 
-    /**
-     * {@code SystemUpdateMessage} builder static inner class.
-     */
     public static final class Builder {
 
         private String locationGroupName;
         private String errorCode;
         private Date created;
 
-        /**
-         * Sets the {@code locationGroupName} and returns a reference to this Builder so that the methods can be chained together.
-         *
-         * @param locationGroupName the {@code locationGroupName} to set
-         * @return a reference to this Builder
-         */
         public Builder withLocationGroupName(String locationGroupName) {
             this.locationGroupName = StringUtils.trimTrailingCharacter(locationGroupName, CommConstants.LOCGROUP_FILLER_CHARACTER);
             return this;
         }
 
-        /**
-         * Add an error code.
-         *
-         * @param errorCode
-         *            The error code
-         * @return The builder
-         */
         public Builder withErrorCode(String errorCode) {
             this.errorCode = errorCode;
             return this;
         }
 
-        /**
-         * Add the date of creation in an expected format as defined in {@link CommConstants#DATE_FORMAT_PATTERN}.
-         *
-         * @param createDate
-         *            The creation date
-         * @return The builder
-         */
         public Builder withCreateDate(String createDate) throws ParseException {
             this.created = asDate(createDate);
             return this;
         }
 
-
-        /**
-         * Returns a {@code SystemUpdateMessage} built from the parameters previously set.
-         *
-         * @return a {@code SystemUpdateMessage} built with parameters of this {@code SystemUpdateMessage.Builder}
-         */
         public SystemUpdateMessage build() {
             SystemUpdateMessage res = new SystemUpdateMessage(this);
             res.setErrorCode(this.errorCode);
@@ -137,11 +100,17 @@ public class SystemUpdateMessage extends Payload implements Serializable {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String asString() {
         return IDENTIFIER + locationGroupName;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "SystemUpdateMessage{" +
